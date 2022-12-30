ed373230eeda10d1e10956227b1b54cdb4100a9e
mrhaomp2001 <huynhnhuthao248@gmail.com>
2022-12-30 22:11:34 +0700

    Dinh nghia cac route cho tai nguyen Contact

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const contactsRouter = require("./app/routes/contact.route");
 
 const app = express();
 
@@ -10,5 +11,7 @@ app.get("/", (req, res) => {
     console.log("hello");
     return res.status(200).json({message: "welcome"});
 });
+
+app.use("/api/contacts", contactsRouter);
 
 module.exports = app;
---
+++ app/controllers/contact.controller.js
@@ -0,0 +1,27 @@
+exports.create = (req, res) => {
+    res.send({message: "create"});
+};
+
+exports.findAll = (req, res) => {
+    res.send({message: "findAll"});
+};
+
+exports.findOne = (req, res) => {
+    res.send({message: "findOne"});
+};
+
+exports.update = (req, res) => {
+    res.send({message: "update"});
+};
+
+exports.delete = (req, res) => {
+    res.send({message: "delete"});
+};
+
+exports.deleteAll = (req, res) => {
+    res.send({message: "deleteAll"});
+};
+
+exports.findAllFavorites = (req, res) => {
+    res.send({message: "findAllFavorites"});
+};
